bea7750d39d4586ee75249b106082bcb60b73604
artem <art.zavtur@gmail.com>
2023-12-14 16:29:26 +0200

    wheel

--- lib/home.dart
+++ lib/home.dart
@@ -1,32 +1,84 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: library_private_types_in_public_api
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Let\'s see what\'s God got for you today!'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+    return ChangeNotifierProvider(
+      create: (_) => WheelNotifier(),
+      builder: (context, _) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Let\'s see what\'s God got for you today!'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        tooltip: 'Increment',
-        label: const Text('Spin'),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.read<WheelNotifier>().spin();
+          },
+          child: const Text('Spin'),
+        ),
+        body: const Wheel(),
       ),
     );
+  }
+}
+
+class Wheel extends StatelessWidget {
+  const Wheel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final items = context.read<WheelNotifier>().items;
+    final controller = context.read<WheelNotifier>().controller1;
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        children: [
+          Expanded(
+            child: FortuneWheel(
+              selected: controller.stream,
+              items: [
+                for (var it in items) FortuneItem(child: Text(it)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WheelNotifier extends ChangeNotifier {
+  StreamController<int> controller1 = StreamController<int>();
+  final items = <String>[
+    'Grogu',
+    'Mace Windu',
+    'Obi-Wan Kenobi',
+    'Han Solo',
+    'Luke Skywalker',
+    'Darth Vader',
+    'Yoda',
+    'Ahsoka Tano',
+  ];
+
+  void spin() {
+    controller1.add(
+      Fortune.randomInt(0, items.length),
+    );
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    controller1.close();
+    super.dispose();
   }
 }
