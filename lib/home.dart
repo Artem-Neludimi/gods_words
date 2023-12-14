15bc883c7ef288412e74ed7bf38f7b979e742086
artem <art.zavtur@gmail.com>
2023-12-14 17:45:56 +0200

    6 wheels

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
 
 import 'dart:async';
 import 'dart:convert';
@@ -18,19 +18,81 @@ class Home extends StatelessWidget {
       create: (_) => WheelNotifier(),
       builder: (context, _) {
         final isLoading = context.watch<WheelNotifier>().isLoading;
+        final controller1 = context.watch<WheelNotifier>().controller1;
+        final controller2 = context.watch<WheelNotifier>().controller2;
+        final controller3 = context.watch<WheelNotifier>().controller3;
+        final controller4 = context.watch<WheelNotifier>().controller4;
+        final controller5 = context.watch<WheelNotifier>().controller5;
+        final controller6 = context.watch<WheelNotifier>().controller6;
+        final items1 = context.watch<WheelNotifier>().items1;
+        final items2 = context.watch<WheelNotifier>().items2;
+        final items3 = context.watch<WheelNotifier>().items3;
+        final items4 = context.watch<WheelNotifier>().items4;
+        final items5 = context.watch<WheelNotifier>().items5;
+        final items6 = context.watch<WheelNotifier>().items6;
         return Scaffold(
           appBar: AppBar(
             title: const Text('Let\'s see what\'s God got for you today!'),
             centerTitle: true,
           ),
           floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () async {
               context.read<WheelNotifier>().spin();
+              await Future.delayed(const Duration(seconds: 5));
+              final word1 = context.read<WheelNotifier>().word1;
+              final word2 = context.read<WheelNotifier>().word2;
+              final word3 = context.read<WheelNotifier>().word3;
+              final word4 = context.read<WheelNotifier>().word4;
+              final word5 = context.read<WheelNotifier>().word5;
+              final word6 = context.read<WheelNotifier>().word6;
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('God says:'),
+                    content: Text(
+                      '${items1![word1]} ${items2![word2]} ${items3![word3]} ${items4![word4]} ${items5![word5]} ${items6![word6]}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
             },
-            child: const Text('Spin'),
+            label: const Text('Spin'),
           ),
-          body: isLoading ? const Center(child: CircularProgressIndicator()) : const Wheel(),
+          body: isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : LayoutBuilder(builder: (context, constraints) {
+                  return GridView(
+                    padding: const EdgeInsets.all(8),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                      childAspectRatio: 1,
+                    ),
+                    children: [
+                      Wheel(items: items1!, controller: controller1),
+                      Wheel(items: items2!, controller: controller2),
+                      Wheel(items: items3!, controller: controller3),
+                      Wheel(items: items4!, controller: controller4),
+                      Wheel(items: items5!, controller: controller5),
+                      Wheel(items: items6!, controller: controller6),
+                    ],
+                  );
+                }),
         );
       },
     );
@@ -38,13 +100,15 @@ class Home extends StatelessWidget {
 }
 
 class Wheel extends StatelessWidget {
-  const Wheel({super.key});
+  const Wheel({super.key, required this.items, required this.controller});
+
+  final List<String> items;
+  final StreamController<int> controller;
 
   @override
   Widget build(BuildContext context) {
-    final items = context.read<WheelNotifier>().items1!;
-    final controller = context.read<WheelNotifier>().controller1;
     return FortuneWheel(
+      animateFirst: false,
       physics: NoPanPhysics(),
       selected: controller.stream,
       items: [
@@ -58,6 +122,7 @@ class Wheel extends StatelessWidget {
             child: Text(
               it,
               style: const TextStyle(
+                fontSize: 11,
                 color: Colors.black,
                 shadows: [
                   Shadow(
@@ -80,22 +145,80 @@ class WheelNotifier extends ChangeNotifier {
       items1 = list.cast<String>();
       notifyListeners();
     });
+    http.get(Uri.parse('https://random-word-api.herokuapp.com/word?number=8')).then((value) {
+      final list = json.decode(value.body) as List<dynamic>;
+      items2 = list.cast<String>();
+      notifyListeners();
+    });
+    http.get(Uri.parse('https://random-word-api.herokuapp.com/word?number=8')).then((value) {
+      final list = json.decode(value.body) as List<dynamic>;
+      items3 = list.cast<String>();
+      notifyListeners();
+    });
+    http.get(Uri.parse('https://random-word-api.herokuapp.com/word?number=8')).then((value) {
+      final list = json.decode(value.body) as List<dynamic>;
+      items4 = list.cast<String>();
+      notifyListeners();
+    });
+    http.get(Uri.parse('https://random-word-api.herokuapp.com/word?number=8')).then((value) {
+      final list = json.decode(value.body) as List<dynamic>;
+      items5 = list.cast<String>();
+      notifyListeners();
+    });
+    http.get(Uri.parse('https://random-word-api.herokuapp.com/word?number=8')).then((value) {
+      final list = json.decode(value.body) as List<dynamic>;
+      items6 = list.cast<String>();
+      notifyListeners();
+    });
   }
-  StreamController<int> controller1 = StreamController<int>();
+  final controller1 = StreamController<int>();
   List<String>? items1;
+  var word1 = Fortune.randomInt(0, 8);
+  final controller2 = StreamController<int>();
+  List<String>? items2;
+  var word2 = Fortune.randomInt(0, 8);
+  final controller3 = StreamController<int>();
+  List<String>? items3;
+  var word3 = Fortune.randomInt(0, 8);
+  final controller4 = StreamController<int>();
+  List<String>? items4;
+  var word4 = Fortune.randomInt(0, 8);
+  final controller5 = StreamController<int>();
+  List<String>? items5;
+  var word5 = Fortune.randomInt(0, 8);
+  final controller6 = StreamController<int>();
+  List<String>? items6;
+  var word6 = Fortune.randomInt(0, 8);
 
-  bool get isLoading => items1 == null;
+  bool get isLoading =>
+      items1 == null || items2 == null || items3 == null || items4 == null || items5 == null || items6 == null;
 
   void spin() {
-    controller1.add(
-      Fortune.randomInt(0, items1!.length),
-    );
+    word1 = Fortune.randomInt(0, items1!.length);
+    word2 = Fortune.randomInt(0, items2!.length);
+    word3 = Fortune.randomInt(0, items3!.length);
+    word4 = Fortune.randomInt(0, items4!.length);
+    word5 = Fortune.randomInt(0, items5!.length);
+    word6 = Fortune.randomInt(0, items6!.length);
+
+    controller1.add(word1);
+    controller2.add(word2);
+    controller3.add(word3);
+    controller4.add(word4);
+    controller5.add(word5);
+    controller6.add(word6);
+
     notifyListeners();
   }
 
   @override
   void dispose() {
     controller1.close();
+    controller2.close();
+    controller3.close();
+    controller4.close();
+    controller5.close();
+    controller6.close();
     super.dispose();
   }
 }
